fd58c92393410d6f8bbe9fc512626658b4931ced
DenKli <https://github.com/DenK1987/AN10-onl>
2023-07-20 17:24:24 +0300

    Lesson-1

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.notesapp.navigation.NotesNavHost
 import com.example.notesapp.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +19,40 @@ class MainActivity : ComponentActivity() {
         setContent {
             NotesAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    NotesNavHost()
+//                }
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "Notes App") },
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.White,
+                            elevation = 12.dp
+                        )
+                    },
+                    content = {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.background
+                        ) {
+                            NotesNavHost()
+                        }
+                    }
+                )
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     NotesAppTheme {
-        Greeting("Android")
+
     }
 }
