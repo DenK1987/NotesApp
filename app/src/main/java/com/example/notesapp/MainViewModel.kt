8211c7ffad15da210518acf5af6396c49ddfe0ce
DenKli <https://github.com/DenK1987/AN10-onl>
2023-08-07 19:58:55 +0300

    Lesson-4

--- app/src/main/java/com/example/notesapp/MainViewModel.kt
+++ app/src/main/java/com/example/notesapp/MainViewModel.kt
@@ -6,38 +6,49 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.notesapp.db.room.AppRoomDatabase
+import com.example.notesapp.db.room.repository.RoomRepository
 import com.example.notesapp.model.Note
 import com.example.notesapp.utils.*
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    val readTest: MutableLiveData<List<Note>> by lazy {
-        MutableLiveData<List<Note>>()
-    }
+//    val readTest: MutableLiveData<List<Note>> by lazy {
+//        MutableLiveData<List<Note>>()
+//    }
+//
+//    private val dbType: MutableLiveData<String> by lazy {
+//        MutableLiveData<String>(TYPE_ROOM)
+//    }
+//
+//    init {
+//        readTest.value =
+//            when (dbType.value) {
+//                TYPE_ROOM -> {
+//                    listOf(
+//                        Note(title = "Note 1", subtitle = "Subtitle for note 1"),
+//                        Note(title = "Note 2", subtitle = "Subtitle for note 2"),
+//                        Note(title = "Note 3", subtitle = "Subtitle for note 3"),
+//                        Note(title = "Note 4", subtitle = "Subtitle for note 4")
+//                    )
+//                }
+//                TYPE_FIREBASE -> listOf()
+//                else -> listOf()
+//            }
+//    }
 
-    private val dbType: MutableLiveData<String> by lazy {
-        MutableLiveData<String>(TYPE_ROOM)
-    }
+    val context = application
 
-    init {
-        readTest.value =
-            when (dbType.value) {
-                TYPE_ROOM -> {
-                    listOf(
-                        Note(title = "Note 1", subtitle = "Subtitle for note 1"),
-                        Note(title = "Note 2", subtitle = "Subtitle for note 2"),
-                        Note(title = "Note 3", subtitle = "Subtitle for note 3"),
-                        Note(title = "Note 4", subtitle = "Subtitle for note 4")
-                    )
-                }
-                TYPE_FIREBASE -> listOf()
-                else -> listOf()
-            }
-    }
-
-    fun initDatabase(type: String) {
-        dbType.value = type
+    fun initDatabase(type: String, onSuccess: () -> Unit) {
+//        dbType.value = type
         Log.d("checkData", "MainViewModel initDatabase with type $type")
+        when (type) {
+            TYPE_ROOM -> {
+                val dao = AppRoomDatabase.getInstance(context = context).getRoomDao()
+                REPOSITORY = RoomRepository(dao)
+                onSuccess()
+            }
+        }
     }
 }
 
